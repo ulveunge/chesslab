6da377a370d7e6ad533a06bb57b7121b955a7f5f
Emin Isaev <emin.isaev97@gmail.com>
2024-11-03 23:09:09 +0700

    small fix

--- src/features/theme-switch/model/store.svelte.ts
+++ src/features/theme-switch/model/store.svelte.ts
@@ -3,7 +3,7 @@ import type { TTheme } from '$shared/lib';
 
 let currentTheme = $state((cookies.get('theme') as TTheme) ?? 'system');
 
-export const changeTheme = (theme: 'light' | 'system' | 'dark') => {
+export const changeTheme = (theme: TTheme) => {
 	document.documentElement.classList.remove(currentTheme);
 	if (theme !== 'system') document.documentElement.classList.add(theme);
 	cookies.set('theme', theme, {
