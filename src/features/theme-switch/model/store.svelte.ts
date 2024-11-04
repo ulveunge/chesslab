8c283fd94cd47b48f98f4270e0d88ba33577b14d
Emin Isaev <emin.isaev97@gmail.com>
2024-11-05 02:54:51 +0700

    refactor theme store

--- src/features/theme-switch/model/store.svelte.ts
+++ src/features/theme-switch/model/store.svelte.ts
@@ -1,23 +1,29 @@
 import cookies from 'js-cookie';
 import type { TTheme } from '$shared/lib';
 
-let currentTheme = $state((cookies.get('theme') as TTheme) ?? 'system');
+const createThemeStore = () => {
+	let currentTheme = $state((cookies.get('theme') as TTheme) ?? 'system');
 
-export const changeTheme = (theme: TTheme) => {
-	document.documentElement.classList.remove(currentTheme);
-	if (theme !== 'system') document.documentElement.classList.add(theme);
-	cookies.set('theme', theme, {
-		path: '/',
-		secure: true,
-		sameSite: 'strict',
-		expires: 3650
-	});
-	currentTheme = theme;
+	const changeTheme = (theme: TTheme) => {
+		document.documentElement.classList.remove(currentTheme);
+		if (theme !== 'system') document.documentElement.classList.add(theme);
+		cookies.set('theme', theme, {
+			path: '/',
+			secure: true,
+			sameSite: 'strict',
+			expires: 3650
+		});
+		currentTheme = theme;
+	};
+
+	return {
+		get currentTheme() {
+			return currentTheme;
+		},
+		changeTheme
+	};
 };
 
-export default {
-	get currentTheme() {
-		return currentTheme;
-	},
-	changeTheme
-};
+const themeStore = createThemeStore();
+
+export default themeStore;
